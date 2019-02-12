452531cc3b754a9092ffaaf414e186e6f9c613a0
alvalle09 <phredvalle@gmail.com>
2019-02-11 21:11:30 -0600

    fixing the autoscroll, but has bug on manual selecting

--- js/scripts.js
+++ js/scripts.js
@@ -7,7 +7,7 @@ $(document).ready(function(){
         positions[i] = totalWidth;
         totalWidth += $(this).width();
 
-        if( !$(this).width()) {
+        if (!$(this).width()) {
             alert('Please add width to images');
             return false;
         }
@@ -24,14 +24,29 @@ $(document).ready(function(){
 
         $('#slides').stop().animate({marginLeft:-positions[pos]+'px'}, 450);
 
+        // prevent default action of anchor link
         e.prevenDefault;
 
         // stop autoscroll
-        if(!autoScroll) clearInterval(itvl);
+        if(!autoScroll) clearInterval(itval);
 
-    })
+    });
 
+    //make first image active
+    $('#menu ul li.product:first').addClass('active').siblings().addClass('inactive');
+    
+    //Auto scroll
+    let current = 1;
 
+    function autoScroll() {
+        if(current == -1 ) return false;
 
+        $('#menu ul li a').eq(current%$('#menu ul li a').length).trigger('click', [true]);
+        current++;
+    }
+
+    // duration for auto-scroll
+    let duration = 2;
+    let itval = setInterval(function() { autoScroll() }, duration * 1000);
 
 })
